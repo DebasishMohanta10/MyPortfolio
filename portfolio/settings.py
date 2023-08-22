0b4dbbff4ba9fe67cc4ee45276186b8d3950de88
DebasishMohanta <debasish2018rta@gmail.com>
2023-08-22 15:04:00 +0530

    Adding static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -107,10 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATICFILES_DIRS = [
